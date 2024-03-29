4457f556de84eebc7b73fa74f25f9dc8e1967be4
abdullah <abullah12@gmail.com>
2024-03-30 00:01:15 +0300

    Initial commit of Spring boot project

---
+++ src/main/java/com/example/springwebdemo/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springwebdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot Web!";
+    }
+}
